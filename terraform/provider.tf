44daca945fca03d7298e49dffd4f62a1dfd74246
bldfrstr <lesnikovdaniel@gmail.com>
2024-02-11 22:06:40 +0300

    Terraform provider configured

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,11 +6,11 @@ terraform {
   }
   required_version = ">= 0.13"
 }
-# provider "yandex" {
-#   token                    = "y0_AgAAAAArD6_QAATuwQAAAAD3WUIZc5NXVh9mQpu2qZo08R3bYo8KWUw"
-#   service_account_key_file = "key.json"
-#   cloud_id                 = "b1g4bn1u29ngf8rj4iga"
-#   folder_id                = "b1g8ar6d0dnaun4184h0"
-#   zone                     = "ru-central1-a"
-# }
+provider "yandex" {
+  token                    = "y0_AgAAAAArD6_QAATuwQAAAAD3WUIZc5NXVh9mQpu2qZo08R3bYo8KWUw"
+  service_account_key_file = "key.json"
+  cloud_id                 = "b1g4bn1u29ngf8rj4iga"
+  folder_id                = "b1g8ar6d0dnaun4184h0"
+  zone                     = "ru-central1-a"
+}
 
