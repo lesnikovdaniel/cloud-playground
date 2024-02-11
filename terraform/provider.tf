44367633e9bfb5698f140ab387b9ae92916092e6
bldfrstr <lesnikovdaniel@gmail.com>
2024-02-11 22:06:10 +0300

    Init commit.Terraform provider configured

---
+++ terraform/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+# provider "yandex" {
+#   token                    = "y0_AgAAAAArD6_QAATuwQAAAAD3WUIZc5NXVh9mQpu2qZo08R3bYo8KWUw"
+#   service_account_key_file = "key.json"
+#   cloud_id                 = "b1g4bn1u29ngf8rj4iga"
+#   folder_id                = "b1g8ar6d0dnaun4184h0"
+#   zone                     = "ru-central1-a"
+# }
+
